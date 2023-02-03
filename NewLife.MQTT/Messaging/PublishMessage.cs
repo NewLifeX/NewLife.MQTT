82910186d2a84c58c3ba804f0789461ba68b1491
大石头 <nnhy@vip.qq.com>
2023-02-03 22:27:17 +0800

    v1.2.2023.0203

--- NewLife.MQTT/Messaging/PublishMessage.cs
+++ NewLife.MQTT/Messaging/PublishMessage.cs
@@ -41,7 +41,7 @@ public sealed class PublishMessage : MqttIdMessage
         }
 
         //Payload = ReadData(stream);
-        Payload = stream.ReadBytes();
+        Payload = stream.ReadBytes(-1);
 
         return true;
     }
